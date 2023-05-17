f7150215232273ff3e5da29bb6b077ae877144f4
monpro <monproshen@gmail.com>
2023-05-17 17:56:15 +1000

    add authenticated & authorized resolvers

--- src/resolvers.js
+++ src/resolvers.js
@@ -8,49 +8,50 @@ const NEW_POST = 'NEW_POST'
  */
 module.exports = {
   Query: {
-    me(_, __, {user}){
+    me: authenticated(authorized('ADMIN', (_, __, {user}) =>{
       return user
-    },
-    posts(_, __, {user, models}) {
+    })),
+
+    posts: authenticated((_, __, {user, models}) => {
       return models.Post.findMany({author: user.id})
-    },
+    }),
 
-    post(_, {id}, {user, models}) {
+    post: authenticated((_, {id}, {user, models}) => {
       return models.Post.findOne({id, author: user.id})
-    },
+    }),
 
-    userSettings(_, __, {user, models}) {
+    userSettings: authenticated((_, __, {user, models}) => {
       return models.Settings.findOne({user: user.id})
-    },
+    }),
     // public resolver
     feed(_, __, {models}) {
       return models.Post.findMany()
     }
   },
   Mutation: {
-    updateSettings(_, {input}, {user, models}) {
+    updateSettings: authenticated((_, {input}, {user, models}) => {
       return models.Settings.updateOne({user: user.id}, input)
-    },
+    }),
 
-    createPost(_, {input}, {user, models}) {
+    createPost: authenticated((_, {input}, {user, models}) => {
       const post = models.Post.createOne({...input, author: user.id})
       pubsub.publish(NEW_POST, { newPost: post })
       return post
-    },
+    }),
 
-    updateMe(_, {input}, {user, models}) {
+    updateMe: authenticated((_, {input}, {user, models}) => {
       return models.User.updateOne({id: user.id}, input)
-    },
+    }),
     // admin role
-    invite(_, {input}, {user}) {
+    invite: authenticated(authorized('ADMIN',(_, {input}, {user}) => {
       return {from: user.id, role: input.role, createdAt: Date.now(), email: input.email}
-    },
+    })),
 
     signup(_, {input}, {models, createToken}) {
       const existing = models.User.findOne({email: input.email})
 
       if (existing) {
-        throw new Error('nope')  
+        throw new Error('nope')
       }
       const user = models.User.createOne({...input, verified: false, avatar: 'http'})
       const token = createToken(user)
@@ -60,7 +61,7 @@ module.exports = {
       const user = models.User.findOne(input)
 
       if (!user) {
-        throw new Error('nope')  
+        throw new Error('nope')
       }
 
       const token = createToken(user)
@@ -70,7 +71,7 @@ module.exports = {
   User: {
     posts(root, _, {user, models}) {
       if (root.id !== user.id) {
-        throw new Error('nope')
+        throw new Error('user id is not matching')
       }
 
       return models.Post.findMany({author: root.id})
